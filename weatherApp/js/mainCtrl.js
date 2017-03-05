173bff0cd625947d4e3e658fa3c7c42dbecb62db
Marcus Eriksson <eriksson.marcus15@gmail.com>
2017-03-05 13:32:59 +0100

    Update mainCtrl.js

--- weatherApp/js/mainCtrl.js
+++ weatherApp/js/mainCtrl.js
@@ -7,7 +7,7 @@ app.controller('mainCtrl', ["$scope", "$http", function($scope, $http) {
       var id = "&appid=YOUR API KEY"; //YOUR API KEY GOES HERE
       var callBack = "&callback=JSON_CALLBACK";
       var apiCall = url + id + callBack; //Concatenates url
-      if($scope.q.length >= 3) {  //If user input greater than 3, send API call
+      if($scope.q.length >= 3) {  //If user input greater than 3, send API call, allows user to search on the 3 first letters of a city
         var promise = $http.jsonp(apiCall);
         promise.success(function(data){
           $scope.data = data;
